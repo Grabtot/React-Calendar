7add248cdd2909c6ffe91f40e37407e4bb415a4d
Grabtot <dneshotkin@gmail.com>
2023-03-31 14:52:58 +0300

    Tasks can display

--- src/components/Calendar/ToDoList/ToDoList.js
+++ src/components/Calendar/ToDoList/ToDoList.js
@@ -4,12 +4,25 @@ import { DateContext } from '../../../contexts/DateContext';
 import { getAll } from '../../../api';
 
 const ToDoList = () => {
+  const { selectedTask } = useContext(DateContext);
 
-  const { selectedDay } = useContext(DateContext);
-  
+  if (!selectedTask.hasOwnProperty("tasks")) {
+    console.log(selectedTask);
+    return (<div className={styles.todo}>
+      <h2>{selectedTask.date.toLocaleDateString()}</h2>
+    </div>);
+  }
+
+  const taskDone = ({ target: value }) => { }
+  const tasks = selectedTask.tasks.map(task => <li key={task.id}>
+    <label>{task.task}</label>
+    <input type='checkbox' value={task.isDone} onChange={taskDone}></input>
+  </li>)
+
   return (
     <div className={styles.todo}>
-      <h2>{selectedDay.getFullYear()} {selectedDay.getMonth()} {selectedDay.getDate()}</h2>
+      <h2>{selectedTask.date}</h2>
+      <ul>{tasks}</ul>
     </div>
   );
 }
