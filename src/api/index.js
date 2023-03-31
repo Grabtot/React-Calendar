d82b2cd786ef7194c78a7450cd3119929aa61d42
Grabtot <dneshotkin@gmail.com>
2023-03-31 16:47:07 +0300

    List works

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,44 @@
-export const getAll = () => {
-  return fetch(`${process.env.PUBLIC_URL}/tasks.json`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Ошибка при загрузке файла tasks.json');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // console.log(data); // Здесь вы увидите данные в виде объекта JSON
-      return data;
-    })
-    .catch((error) => {
-      console.error('Произошла ошибка:', error);
-    });
+import axios from 'axios';
+
+export const getAll = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/todos');
+    return response.data;
+  } catch (error) {
+    console.error('Произошла ошибка:', error);
+    throw new Error('Ошибка при загрузке данных с сервера');
+  }
+};
+
+
+export const addNew = async (newDate) => {
+  try {
+    const response = await axios.post('http://localhost:5000/todos', newDate);
+    return response.data;
+  } catch (error) {
+    console.error('Произошла ошибка:', error);
+    throw new Error('Ошибка при добавлении новой задачи');
+  }
+};
+
+// Обновление данных
+export const update = async (id, updatedData) => {
+  try {
+    const response = await axios.put(`http://localhost:5000/todos/${id}`, updatedData);
+    return response.data;
+  } catch (error) {
+    console.error('Произошла ошибка:', error);
+    throw new Error('Ошибка при обновлении данных на сервере');
+  }
+};
+
+// Удаление данных
+export const remove = async (id) => {
+  try {
+    const response = await axios.delete(`http://localhost:5000/todos/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Произошла ошибка:', error);
+    throw new Error('Ошибка при удалении данных на сервере');
+  }
 };
